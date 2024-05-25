6c680d438273307e019d8557e2666561a65fdc8c
squilliam34 <wdfan0128@gmail.com>
2024-05-25 15:23:19 -0400

    made ball larger

--- src/components/TennisBall.tsx
+++ src/components/TennisBall.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const TennisBall: React.FC = () => (
   <div className="center">
     <div className="ball flex centered-words font">
-      <div>I'm Will</div>
-      <div className="red">Welcome to my page!</div>
+      <div className="medium-font">I'm Will</div>
+      <div className="red medium-font">Welcome to my page!</div>
     </div>
     <div className="shadow"></div>
   </div>
