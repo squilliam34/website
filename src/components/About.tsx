fa3666269e2b977b6a01f334dc61f94addc297b2
squilliam34 <wdfan0128@gmail.com>
2024-05-25 09:26:22 -0400

    different colors for github and linkedin

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,14 +19,14 @@ const About: React.FC = () => (
         <a
           href="https://github.com/squilliam34"
           target="_blank"
-          className="social-link"
+          className="github"
         >
           <i className="fab fa-github"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/william-fan-376159265/"
           target="_blank"
-          className="social-link"
+          className="linkedin"
         >
           <i className="fab fa-linkedin"></i>
         </a>
