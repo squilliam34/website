58e9f9f61a3bc96aeb0a0361f6c184d92bce121f
squilliam34 <wdfan0128@gmail.com>
2024-05-25 14:58:38 -0400

    tennis ball styles

--- src/pages/About/aboutPage.tsx
+++ src/pages/About/aboutPage.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import Header from "../../components/Header";
 import About from "../../components/About";
 import Slideshow from "../../components/Slideshow";
+import TennisBall from "../../components/TennisBall";
 import "../../css/styles.css";
+import "../../css/tennis.css";
 
 const App: React.FC = () => (
   <div>
     <Header />
     <main>
+      <div className="margin-bottom">
+        <TennisBall />
+      </div>
       <div className="flex wrapper">
         <About />
         <Slideshow />
