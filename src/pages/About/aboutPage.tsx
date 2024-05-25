cce3b625014411c314be4d949b7f3b690162e72a
squilliam34 <wdfan0128@gmail.com>
2024-05-25 18:46:26 -0400

    css for scroll button

--- src/pages/About/aboutPage.tsx
+++ src/pages/About/aboutPage.tsx
@@ -4,8 +4,10 @@ import Header from "../../components/Header";
 import About from "../../components/About";
 import Slideshow from "../../components/Slideshow";
 import TennisBall from "../../components/TennisBall";
+import ScrollButton from "../../components/ScrollButton";
 import "../../css/styles.css";
 import "../../css/tennis.css";
+import "../../css/scoll.css";
 
 const App: React.FC = () => (
   <div>
@@ -13,6 +15,9 @@ const App: React.FC = () => (
     <main>
       <div>
         <TennisBall />
+        <div className="about-button">
+          <ScrollButton />
+        </div>
       </div>
       <div className="flex wrapper margin-bottom">
         <About />
