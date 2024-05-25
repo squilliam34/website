9eef5d9b043859244c50a8748e07d64bc66ea626
squilliam34 <wdfan0128@gmail.com>
2024-05-25 14:58:03 -0400

    tennis ball component

---
+++ src/components/TennisBall.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TennisBall: React.FC = () => (
+  <div className="center">
+    <div className="ball flex centered-words font">
+      <div>I'm Will</div>
+      <div className="red">Welcome to my page!</div>
+    </div>
+    <div className="shadow"></div>
+  </div>
+);
+
+export default TennisBall;
