c6e65e1ea789b18f9781d7a222ad5a68a8619660
squilliam34 <wdfan0128@gmail.com>
2024-05-26 00:15:43 -0400

    paragraph semantics

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -11,9 +11,9 @@ const AboutMe: React.FC = () => (
         I'm currently a junior at the College of William and Mary, and I'm
         pursuing a Bachelor of Science with a double major in Computer Science
         and Applied Mathematics. I built this website to showcase my
-        experiences, projects, and a little of my personality, too. Feel free to
-        take a look around! Below are my Github and LinkedIn if you want to
-        connect with me or look at other work of mine (or you can{" "}
+        experiences, projects, and a little bit of my personality, too. Feel
+        free to take a look around! Below are my Github and LinkedIn if you want
+        to connect with me or look at other work of mine (or you can{" "}
         <a href="mailto:wdfan0128@gmail.com">email me here!</a>)
       </p>
       <div className="flex margin-top">
