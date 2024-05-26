7083f149a13c1f890f456ca0a8afe628e6c4b634
squilliam34 <wdfan0128@gmail.com>
2024-05-25 21:56:47 -0400

    index for easy imports

---
+++ src/components/About/index.ts
@@ -0,0 +1,6 @@
+export { default as AboutMe } from "./AboutMe";
+export { default as Header } from "./Header";
+export { default as MoreAboutMe } from "./MoreAboutMe";
+export { default as ScrollButton } from "./ScrollButton";
+export { default as  Slideshow} from "./Slideshow";
+export { default as  TennisBall} from "./TennisBall";
