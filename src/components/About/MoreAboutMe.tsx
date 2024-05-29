cfa88db38d69e470d25aa4e4542b4dec424c3e52
squilliam34 <wdfan0128@gmail.com>
2024-05-29 01:02:35 -0400

    MoreAboutMe content

--- src/components/About/MoreAboutMe.tsx
+++ src/components/About/MoreAboutMe.tsx
@@ -3,8 +3,16 @@ import React from "react";
 const MoreAboutMe: React.FC = () => {
   return (
     <div className="header margin-left margin-right">
-      <h1 className="medium-font">About Me</h1>
-      <p className="line-height">I've always been a numbers guy.</p>
+      <h1 className="medium-font">
+        I love solving <span className="tennis-yellow">problems</span>
+      </h1>
+      <p className="line-height">
+        That's what drew me to coding in the first place. Whether it was on the
+        tennis court or in the classroom, finding solutions has always been my
+        passion. I've come to love programming and all that comes with it,
+        whether that's finding a bug, implementing Djikstra's algorithm, or
+        designing websites, and I can't wait to see where it takes me.
+      </p>
     </div>
   );
 };
