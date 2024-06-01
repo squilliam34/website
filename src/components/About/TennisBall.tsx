5312009bd34288eac6dda75090688e6ea0440d18
squilliam34 <wdfan0128@gmail.com>
2024-06-01 18:01:27 -0400

    change in style name

--- src/components/About/TennisBall.tsx
+++ src/components/About/TennisBall.tsx
@@ -4,7 +4,7 @@ const TennisBall: React.FC = () => {
   return (
     <div className="center">
       <div className="ball flex centered-words font">
-        <div className="black medium-font">I'm Will</div>
+        <div className="navy medium-font">I'm Will</div>
         <div className="red medium-font">Welcome to my page!</div>
       </div>
       <div className="shadow"></div>
