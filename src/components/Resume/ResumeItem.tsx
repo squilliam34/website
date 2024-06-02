72f436f0fff58b6a5f421e178fcaedee65a82b90
squilliam34 <wdfan0128@gmail.com>
2024-06-02 00:15:14 -0400

    styling, formatting, functionality for dropdown

--- src/components/Resume/ResumeItem.tsx
+++ src/components/Resume/ResumeItem.tsx
@@ -1,1 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface ResumeProps {
+  position: string;
+  timeline: string;
+  company: string;
+  description: string;
+}
+
+const ResumeItem: React.FC<ResumeProps> = ({
+  position,
+  timeline,
+  company,
+  description,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div className="flex">
+        <div onClick={toggleDropdown} className="pointer">
+          <span className="dropdown-icon navy">{isOpen ? "▾" : "▸"}</span>
+        </div>
+        <div className="flex resume-line">
+          <div className="position">{position}</div>
+
+          <div className="timeline">{timeline}</div>
+        </div>
+      </div>
+      <div className="company">{company}</div>
+      {isOpen && <div>{description}</div>}
+    </div>
+  );
+};
+
+export default ResumeItem;
