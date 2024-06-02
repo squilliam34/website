438ec623aab5f8b6b7518b2b3fac9dbe0095c0de
squilliam34 <wdfan0128@gmail.com>
2024-06-02 00:14:53 -0400

    initial webster bank commit

--- src/components/Resume/Experiences.tsx
+++ src/components/Resume/Experiences.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ResumeItem from "./ResumeItem";
 
 const Experiences: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="medium-margin-top">
+      <h1>Work Experiences</h1>
+      <div className="flex column box">
+        <div id="webster">
+          <ResumeItem
+            position="Private Banking Analyst"
+            timeline="June 2024 - August 2024"
+            company="Webster Bank"
+            description="blah"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experiences;
