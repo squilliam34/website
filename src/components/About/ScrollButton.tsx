7992115b082b68c5211f1569e12d88da7bf513c8
squilliam34 <wdfan0128@gmail.com>
2024-06-01 21:53:43 -0400

    import change

--- src/components/About/ScrollButton.tsx
+++ src/components/About/ScrollButton.tsx
@@ -1,15 +1,15 @@
-import { Link as ScrollLink } from "react-scroll";
+import { Link } from "react-scroll";
 import { FiChevronsDown } from "react-icons/fi";
 
 const ScrollButton: React.FC = () => {
   return (
     <div className="flex center-align column">
-      <ScrollLink to="about-me" smooth={true} duration={500} offset={-300}>
+      <Link to="about-me" smooth={true} duration={500} offset={-300}>
         <div className="flex center-align column">
           <div>About Me</div>
           <FiChevronsDown size={20} className="text-gray-600" />
         </div>
-      </ScrollLink>
+      </Link>
     </div>
   );
 };
