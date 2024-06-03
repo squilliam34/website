1f2a88db7d3ebbf0b981f4ca1e8cd46126e8160e
squilliam34 <wdfan0128@gmail.com>
2024-06-02 21:56:56 -0400

    added dominguez wealth management experience

--- src/components/Resume/Experiences.tsx
+++ src/components/Resume/Experiences.tsx
@@ -14,6 +14,14 @@ const Experiences: React.FC = () => {
             description="blah"
           />
         </div>
+        <div id="dominguez" className="border">
+          <ResumeItem
+            position="Office Intern"
+            timeline="June 2023-August 2023"
+            company="Dominguez Wealth Management"
+            description="Participated in and contributed to the daily operations of the firm - Attended meetings to discuss clients’ desired risk in their portfolio and determine their account distribution as well as aided fixed income calculations by calculating social security in Excel, engaged with wholesalers and took notes on their various ETFs and their composition, implemented a system to organize notes for hundreds of clients’ meetings, calls, reviews, and plans"
+          />
+        </div>
       </div>
     </div>
   );
