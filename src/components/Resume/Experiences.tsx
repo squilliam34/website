9de52e93b0d0cbfb0a163824a3b29d389b1c9ed3
squilliam34 <wdfan0128@gmail.com>
2024-06-02 22:04:46 -0400

    added second pillar consulting experience

--- src/components/Resume/Experiences.tsx
+++ src/components/Resume/Experiences.tsx
@@ -17,9 +17,17 @@ const Experiences: React.FC = () => {
         <div id="dominguez" className="border">
           <ResumeItem
             position="Office Intern"
-            timeline="June 2023-August 2023"
+            timeline="June 2023 - August 2023"
             company="Dominguez Wealth Management"
             description="Participated in and contributed to the daily operations of the firm - Attended meetings to discuss clients’ desired risk in their portfolio and determine their account distribution as well as aided fixed income calculations by calculating social security in Excel, engaged with wholesalers and took notes on their various ETFs and their composition, implemented a system to organize notes for hundreds of clients’ meetings, calls, reviews, and plans"
+          />
+        </div>
+        <div id="spc" className="border">
+          <ResumeItem
+            position="Python Developer Intern"
+            timeline="May 2023 - August 2023"
+            company="Second Pillar Consulting"
+            description="Collaborated in the development of climate change forecasts for financial institutions to conduct stress testing - Wrote Python and Scala modules to implement technical indicators and metrics as described in published research, ensured code was optimized for processing 30+ TBs of climate data, used Sphinx module to write documentation to enhance code readability and usability, implemented unit tests to ensure code is accurate"
           />
         </div>
       </div>
