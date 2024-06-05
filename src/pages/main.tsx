d8cb8af6d8631632f5191339b0a368174d5695a7
squilliam34 <wdfan0128@gmail.com>
2024-06-04 22:51:41 -0400

    added skills commit

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,6 +5,7 @@ import "../css/tennis.css";
 import "../css/scroll.css";
 import "../css/sidebar.css";
 import "../css/resume.css";
+import "../css/skills.css"
 import "../css/footer.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
