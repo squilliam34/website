d63f15fec41a9794c542c4f959511d38ad0e3d0a
squilliam34 <wdfan0128@gmail.com>
2024-06-24 23:44:19 -0400

    redundant styling

--- src/components/About/ScrollButton.tsx
+++ src/components/About/ScrollButton.tsx
@@ -3,11 +3,11 @@ import { FiChevronsDown } from "react-icons/fi";
 
 const ScrollButton: React.FC = () => {
   return (
-    <div className="flex center-align column">
+    <div>
       <Link to="about-me" smooth={true} duration={500} offset={-300}>
         <div className="flex center-align column">
           <div>About Me</div>
-          <FiChevronsDown size={20} className="text-gray-600" />
+          <FiChevronsDown size={20} />
         </div>
       </Link>
     </div>
