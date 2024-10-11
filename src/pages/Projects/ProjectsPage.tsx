7bdb98161cd2434e151c0a63089944d6d6d07dbc
squilliam34 <wdfan0128@gmail.com>
2024-10-11 02:56:47 -0400

    projects page

---
+++ src/pages/Projects/ProjectsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProjectsGrid from "../../components/Projects/ProjectsGrid";
+
+const ProjectsPage: React.FC = () => {
+  return (
+    <div>
+      <ProjectsGrid />
+    </div>
+  );
+};
+
+export default ProjectsPage;
